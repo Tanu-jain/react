782ef28b7c2e87e7ae75ef7b7a4e86c9af5e9f18
Tanu-jain <tanujaintj1@gmail.com>
2020-07-20 16:30:48 +0530

    Updates

---
+++ precache-manifest.da5c02fb73c59531817b2bbaec2675bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32f605fcfcccc6c51f8845dd8866bd75",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "87bc8cdba0043e09e933",
+    "url": "/react/static/css/main.6fc8f346.chunk.css"
+  },
+  {
+    "revision": "5db7f2d4b03ed35435c7",
+    "url": "/react/static/js/2.03b3a3f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.03b3a3f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87bc8cdba0043e09e933",
+    "url": "/react/static/js/main.94267730.chunk.js"
+  },
+  {
+    "revision": "437bea7043ecbf308edf",
+    "url": "/react/static/js/runtime-main.565b0207.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
